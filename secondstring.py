db0b3d621fb5d1c3864ee5bdb9538d1b6a753241
unknown <fabio.fabrizi@gmail.com>
2022-03-26 13:18:02 +0000

    Made a function with the loop inside it and error checking for the user input

--- secondstring.py
+++ secondstring.py
@@ -3,14 +3,37 @@
 # Author: Fabio Fabrizi
 
 # Steps (I did paper and pen first)
-# Find Python function to reverse a string - There is none!
-# 
-
-
+#
 # Found the solution from Googling - W3 schools.
 # https://www.w3schools.com/python/python_howto_reverse_string.asp 
 # Using slice, changed the step from -1 to -2
 # txt = "The quick brown fox jumps over the lazy dog."[::-2]
 # print(txt)
-txt = input("Please enter some text: ")
-print(txt[::-2])
+# This was the first version
+# txt = input("Please enter some text: ")
+# print(txt[::-2])
+
+# Prompt the user for input, whilst checking if the user 
+# has actually entered a sentence using a loop.
+# The loop will always run whilst the input is empty, 
+# so the user is forced to enter something.
+txt =""
+while len(txt) == 0:
+    txt = input("Please enter a sentence: ")
+    if len(txt) == 0:
+        print("If you don't enter a sentence, it can't be reversed!")
+
+# Function to reverse the string
+# Notice the index is counting back every 2 
+def second_string(txt):
+    # Set result to empty string
+    result = ""
+    # Find index of last char with len(txt)
+    index = len(txt) - 1
+    while index >= 0:
+        result += txt[index]
+        index -= 2
+    return result
+second_string(txt)
+# Below prints the result
+print(second_string(txt))
